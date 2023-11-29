d12ec190c23fb99e43774c49881a669dd8233ddd
Maxwell Riesel <maxwellriesel@yahoo.com>
2023-11-29 17:34:30 -0500

    define files in route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
-import React from 'react';
-import AboutPage from "./components/AboutPage";
-import './App.css';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/Header"
 import Footer from "./components/Footer";
-
+import HomePage from "./components/Homepage";
+import AboutPage from "./components/AboutPage";
+import IndustryPage from "./components/IndustryPage";
+import ServicesPage from "./components/ServicesPage";
+import ContactPage from "./components/ContactPage";
+import SentPage from "./components/SentPage";
+import "./App.css";
 
 function App() {
   return (
